c7f79071afc7cde3dd5dbc1079a2eb37c81c0a32
Tyler <tylerw@live.com>
2020-11-09 23:14:38 -0800

    Cleaned up and revised comments.

--- bullet.py
+++ bullet.py
@@ -1,3 +1,4 @@
+# This class represnts a bullet shot into space.
 import pygame
 import math
 
@@ -5,34 +6,30 @@ import math
 bulletSurf = pygame.Surface((2, 2))
 bulletSurf.fill((255, 255, 255))
 
-# Set a color key for blitting
-bulletSurf.set_colorkey((255, 0, 0))
-
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, velocity, position):
-        # initialize the super sprite class
+        # Initialize the super sprite class
         super(Bullet, self).__init__()
 
-        # Give a velocity in same direction as the ship
-        # Double the speed
+        # Give a velocity in same direction as the ship with specified speed.
         self.speed = 6
         self.velocity = self.normalize_vector(velocity)
         self.velocity = self.scale_vector(self.velocity, self.speed)
 
-        # Face direction of velocity if there is length
-
-        # Give surface
+        # Take the default surface and set to initial position.
         self.surf = bulletSurf
         self.rect = self.surf.get_rect(center=position)
 
+        # Lifespan of the bullet in frames
         self.lifespan = 120
 
+    # Update function to move the bullet through space
     def update(self):
         # Move by the velocity
         self.rect.move_ip(self.velocity)
 
-        # Loopscreen
+        # Loops the edges of the screen
         if(self.rect.right < 0):
             self.rect.left = 800
         if(self.rect.left > 800):
@@ -42,8 +39,9 @@ class Bullet(pygame.sprite.Sprite):
         if(self.rect.bottom < 0):
             self.rect.top = 800
 
-        # Kill if been around for all frames in lifespan (60fps)
-        if self.lifespan < 0:
+        # Kill the bullet if the lifespan is zero, otherwise, decrement it by
+        # one frame
+        if self.lifespan <= 0:
             self.kill()
         else:
             self.lifespan -= 1
