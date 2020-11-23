580b7d80c7bace3f36e4b797cdb029624c65bd87
Tyler <tylerw@live.com>
2020-11-22 19:19:29 -0800

    Gave the player a sprite.

--- player.py
+++ player.py
@@ -6,19 +6,9 @@ from pygame.locals import (
     K_LEFT,
     K_RIGHT,
 )
-
-# Create a surface that will represent the player
-playerSurf = pygame.Surface((20, 10))
-playerSurf.fill((255, 255, 255))
-
-# set a color key for blitting
-playerSurf.set_colorkey((255, 0, 0))
-
-# create shapes so you can tell rotation is happenning
-smaller = pygame.Rect(0, 0, 5, 10)
-
-# draw those two shapes to that surface
-pygame.draw.rect(playerSurf, (75, 255, 75), smaller)
+# Load the sprite images for the ship
+shipImages = [pygame.image.load('sprites/player_ship_idle.png'),
+              pygame.image.load('sprites/player_ship_thrust.png')]
 
 
 # Define our player object and call super to give it all the properties and
@@ -27,23 +17,28 @@ pygame.draw.rect(playerSurf, (75, 255, 75), smaller)
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = playerSurf
+        self.isThrust = False
+        self.surf = self.makeSurface()
         self.rect = self.surf.get_rect()
         self.velocity = Vector2D(0, 0)
 
     def update(self, pressed_keys):
         # Move following the velocity vector
         self.rect.move_ip(self.velocity.to_tuple())
-
+        self.isThrust = False
         # Apply acceleration to velocity
         a = 0.1
         if pressed_keys[K_UP]:
+            self.isThrust = True
             self.velocity.add(Vector2D(0, -a))
         if pressed_keys[K_DOWN]:
+            self.isThrust = True
             self.velocity.add(Vector2D(0, a))
         if pressed_keys[K_LEFT]:
+            self.isThrust = True
             self.velocity.add(Vector2D(-a, 0))
         if pressed_keys[K_RIGHT]:
+            self.isThrust = True
             self.velocity.add(Vector2D(a, 0))
 
         # Damping the velocity
@@ -54,13 +49,13 @@ class Player(pygame.sprite.Sprite):
 
         # Loop the screen
         if self.rect.right < 0:
-            self.rect.left = 800
-        elif self.rect.left > 800:
+            self.rect.left = 1920
+        elif self.rect.left > 1920:
             self.rect.right = 0
-        if self.rect.top > 800:
+        if self.rect.top > 1080:
             self.rect.bottom = 0
         elif self.rect.bottom < 0:
-            self.rect.top = 800
+            self.rect.top = 1080
 
     def rotate(self, angle):
         # save the old center postion
@@ -68,8 +63,21 @@ class Player(pygame.sprite.Sprite):
 
         # sets the current surface to the enemy surface rotated to the
         # indicated angle
-        self.surf = pygame.transform.rotate(playerSurf, angle)
+        self.surf = pygame.transform.rotate(self.makeSurface(), angle+90)
 
         # get the rect of the rotated surf and set it's center to the saved
         self.rect = self.surf.get_rect()
         self.rect.center = oldCenter
+
+    def makeSurface(self):
+        # Create a surface that will represent the player
+        playerSurf = pygame.Surface((64, 64))
+
+        # blit the image onto the surface
+        playerSurf.set_colorkey((255, 0, 0))
+        if self.isThrust:
+            playerSurf.blit(shipImages[1], (0, 0))
+        else:
+            playerSurf.blit(shipImages[0], (0, 0))
+
+        return playerSurf
