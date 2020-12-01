b2cde8a66f22019f36807fd0eee8a2d18c496c56
Tyler <tylerw@live.com>
2020-12-01 02:54:36 -0800

    Added replayability and score history support.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from vector2d import Vector2D
 # External imports
 import pygame
 import math
+import json
 from pygame.locals import (
     K_ESCAPE,
     K_SPACE,
@@ -35,15 +36,12 @@ screen = pygame.display.set_mode((1920, 1080))
 background = pygame.Surface(screen.get_size())
 background.blit(backgroundImage, (0, 0))
 
-# Instantiate the player
-player = Player()
 
 # Makes groups of sprites that have built in methods such as collision
 # detection
 enemies = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
-all_sprites.add(player)
 
 # Add a new eney every 250 miliseconds with custom event detection and timer
 ADDENEMY = pygame.USEREVENT + 1
@@ -51,7 +49,27 @@ pygame.time.set_timer(ADDENEMY, 200)
 
 # Setup the clock to limit framerate
 clock = pygame.time.Clock()
-running = False
+
+
+def best_score_rank(points):
+    filename = 'scores.json'
+    try:
+        # Open the scores and add new entry
+        with open(filename) as f:
+            scores = json.load(f)
+            scores.append(points)
+            scores.sort(reverse=True)
+    # Make file if there is no entry
+    except FileNotFoundError:
+        scores = [points]
+        with open(filename, 'w') as f:
+            json.dump(scores, f)
+        return(score, 1)
+    else:
+        # Save the new entry
+        with open(filename, 'w') as f:
+            json.dump(scores, f)
+        return(scores[0], scores.index(points)+1)
 
 
 # Used to fire the lasers from the guns on the ship sprite
@@ -89,99 +107,175 @@ actionFont = pygame.font.SysFont('hack', 72)
 play = actionFont.render("Press 'enter' to play", True, (173, 185, 204))
 play_rect = play.get_rect(center=(1920/2, 1080/2))
 
+# God Loop
+running = True
+while running:
 
-# Menu Loop
-while running is False:
-    # Loops through the event queue.
-    for event in pygame.event.get():
-        # Quit if the user clicks the quit button.
-        if event.type == QUIT:
-            pygame.quit()
-        # Looks for a key pressed event.
-        elif event.type == KEYDOWN:
-            # Quit if the escape key is pressed.
-            if event.key == K_ESCAPE:
+    # Menu Loop
+    menu = True
+    while menu:
+        # Loops through the event queue.
+        for event in pygame.event.get():
+            # Quit if the user clicks the quit button.
+            if event.type == QUIT:
                 pygame.quit()
-            # Start the game if enter is pressed
-            if event.key == K_RETURN:
-                running = True
-        elif event.type == ADDENEMY and len(enemies) < 50:
-            new_enemy = Enemy()
-            enemies.add(new_enemy)
-            all_sprites.add(new_enemy)
+            # Looks for a key pressed event.
+            elif event.type == KEYDOWN:
+                # Quit if the escape key is pressed.
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                # Start the game if enter is pressed
+                if event.key == K_RETURN:
+                    menu = False
+            elif event.type == ADDENEMY and len(enemies) < 50:
+                new_enemy = Enemy()
+                enemies.add(new_enemy)
+                all_sprites.add(new_enemy)
 
-    # Paint the background
-    screen.blit(background, (0, 0))
+        # Paint the background
+        screen.blit(background, (0, 0))
 
-    enemies.update(enemies, (-1, -1))  # Ignores the player
+        enemies.update(enemies, (-1, -1))  # Ignores the player
+
+        for enemy in enemies:
+            screen.blit(enemy.surf, enemy.rect)
+
+        screen.blit(title, title_rect)
+        screen.blit(play, play_rect)
+
+        pygame.display.flip()
+
+        # Ensure program maintains a rate of 60 frames per second
+        clock.tick(60)
 
     for enemy in enemies:
-        screen.blit(enemy.surf, enemy.rect)
+        enemy.kill()
 
-    screen.blit(title, title_rect)
-    screen.blit(play, play_rect)
+    # Instantiate the player
+    player = Player()
+    points = 0
+    multiplier = 1
+    all_sprites.add(player)
 
-    pygame.display.flip()
+    scoreFont = pygame.font.SysFont('hack', 24)
+    score = scoreFont.render("score: " + str(points), True, (173, 185, 204))
+    score_rect = score.get_rect(right=1910, top=10)
 
-    # Ensure program maintains a rate of 60 frames per second
-    clock.tick(60)
+    # Game loop!
+    playing = True
+    while playing:
+        # Loops through the event queue.
+        for event in pygame.event.get():
+            # Quit if the user clicks the quit button.
+            if event.type == QUIT:
+                pygame.quit()
+            # Looks for a key pressed event.
+            elif event.type == KEYDOWN:
+                # Quit if the escape key is pressed.
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                # Fire a bullet from the player if space is pressed
+                if event.key == K_SPACE:
+                    fireLasers()
+            # Custom event detection to add enemy
+            elif event.type == ADDENEMY and len(enemies) < 20:
+                new_enemy = Enemy()
+                enemies.add(new_enemy)
+                all_sprites.add(new_enemy)
 
-for enemy in enemies:
-    enemy.kill()
+        # Paint the background
+        screen.blit(background, (0, 0))
 
-# Game loop!
-while running:
-    # Loops through the event queue.
-    for event in pygame.event.get():
-        # Quit if the user clicks the quit button.
-        if event.type == QUIT:
-            running = False
-        # Looks for a key pressed event.
-        elif event.type == KEYDOWN:
-            # Quit if the escape key is pressed.
-            if event.key == K_ESCAPE:
-                running = False
-            # Fire a bullet from the player if space is pressed
-            if event.key == K_SPACE:
-                fireLasers()
-        # Custom event detection to add enemy
-        elif event.type == ADDENEMY and len(enemies) < 20:
-            new_enemy = Enemy()
-            enemies.add(new_enemy)
-            all_sprites.add(new_enemy)
+        # Updates entities in these groups, providing necessary information
+        pressed_keys = pygame.key.get_pressed()
+        player.update(pressed_keys)
+        enemies.update(enemies, player.rect.center)
+        bullets.update()
 
-    # Paint the background
-    screen.blit(background, (0, 0))
+        # Draws all sprites to the screen
+        for entity in all_sprites:
+            screen.blit(entity.surf, entity.rect)
 
-    # Updates entities in these groups, providing necessary information
-    pressed_keys = pygame.key.get_pressed()
-    player.update(pressed_keys)
-    enemies.update(enemies, player.rect.center)
-    bullets.update()
+        # Kill the player if he collides with an enemy
+        if pygame.sprite.spritecollideany(player, enemies):
+            # Make a new player when the old one dies
+            player.kill()
+            playing = False
 
-    # Draws all sprites to the screen
-    for entity in all_sprites:
-        screen.blit(entity.surf, entity.rect)
+        # Kill the enemy and the bullet if the bullet collides with an enemy
+        for bullet in bullets:
+            for enemy in enemies:
+                if pygame.sprite.collide_rect(bullet, enemy):
+                    bullet.kill()
+                    enemy.kill()
+                    points += multiplier
+                    multiplier += 1
+                    # Rerender the score text
+                    score = scoreFont.render("Score: " + str(points), True, (173, 185, 204))
+                    score_rect = score.get_rect(right=1910, top=10)
 
-    # Kill the player if he collides with an enemy
-    if pygame.sprite.spritecollideany(player, enemies):
-        # Make a new player when the old one dies
-        player.kill()
-        player = Player()
-        all_sprites.add(player)
+        screen.blit(score, score_rect)
 
-    # Kill the enemy and the bullet if the bullet collides with an enemy
-    for bullet in bullets:
-        for enemy in enemies:
-            if pygame.sprite.collide_rect(bullet, enemy):
-                bullet.kill()
-                enemy.kill()
+        # Update the display to see new drawings
+        pygame.display.flip()
 
-    # Update the display to see new drawings
-    pygame.display.flip()
+        # Ensure program maintains a rate of 60 frames per second
+        clock.tick(60)
 
-    # Ensure program maintains a rate of 60 frames per second
-    clock.tick(60)
+    # Find the best score and the rank of current score and add to file
+    scoreAndRank = best_score_rank(points)  # (bestScore, Rank)
+
+    # Text used to show the death message
+    deathFont = pygame.font.SysFont('hack', 144)
+    death = deathFont.render('Game Over', True, (173, 185, 204))
+    death_rect = death.get_rect(center=(1920/2, 1080/4))
+
+    finalScoreFont = pygame.font.SysFont('hack', 72)
+    finalScore = finalScoreFont.render(
+        "Score: " + str(points) + "    Rank: " + str(scoreAndRank[1]), True, (173, 185, 204))
+    finalScore_rect = finalScore.get_rect(center=(1920/2, 1080/2))
+
+    bestScoreFont = pygame.font.SysFont('hack', 72)
+    bestScore = bestScoreFont.render("Best: " + str(scoreAndRank[0]), True, (173, 185, 204))
+    bestScore_rect = bestScore.get_rect(center=(1920/2, 1080*3/4))
+
+    # Death Screen
+    endScreen = True
+    while endScreen:
+        # Loops through the event queue.
+        for event in pygame.event.get():
+            # Quit if the user clicks the quit button.
+            if event.type == QUIT:
+                pygame.quit()
+            # Looks for a key pressed event.
+            elif event.type == KEYDOWN:
+                # Quit if the escape key is pressed.
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                # Restart the game game if enter is pressed
+                if event.key == K_RETURN:
+                    endScreen = False
+
+        # Paint the background
+        screen.blit(background, (0, 0))
+
+        bullets.update()
+        enemies.update(enemies, (-1, -1))  # Ignores the player
+
+        for sprite in all_sprites:
+            screen.blit(sprite.surf, sprite.rect)
+
+        screen.blit(death, death_rect)
+        screen.blit(finalScore, finalScore_rect)
+        screen.blit(bestScore, bestScore_rect)
+
+        pygame.display.flip()
+
+        # Ensure program maintains a rate of 60 frames per second
+        clock.tick(60)
+
+    for enemy in enemies:
+        enemy.kill()
 
 # Quits the program when the game loop ends.
 pygame.quit()
